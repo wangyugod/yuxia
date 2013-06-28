d4ec6262d33e6b8eda844523995593b5a6e6dc28
wangyugod <wangyu4j@gmail.com>
2013-06-28 08:19:17 +0800

    commit home change, morning for merchandise part

---
+++ app/controllers/Merchandise.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api.mvc.Controller
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-6-28
+ * Time: 上午8:17
+ * To change this template use File | Settings | File Templates.
+ */
+object Merchandise extends Controller{
+
+}
--- app/models/Category.scala
+++ app/models/Category.scala
@@ -11,9 +11,13 @@ import java.sql.Date
  */
 case class Category(id:String, name:String, description:String, longDescription:String)
 
-case class Product(id:String, name:String, description:String, longDescription:String, startDate:Date, endDate:Date, merchantId:String, categoryId:String, template:String)
+case class Product(id:String, name:String, description:String, longDescription:String, startDate:Date, endDate:Date, merchantId:String, template:String)
+
+case class ProductCategory(productId:String, categoryId:String)
 
 case class Sku(id:String, name:String, description:String, longDescription:String, size:Int, productId:String)
+
+case class ProductSku(skuId:String, productId:String)
 
 case class Media(id:String, name:String, description:String, url:String)
 
