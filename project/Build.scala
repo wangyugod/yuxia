535785e693c5da11ef56f4ed5b7d282dd69da3f7
Simon Wang <wangyu4j@gmail.com>
2013-08-23 13:22:23 +0800

    dynatree

--- project/Build.scala
+++ project/Build.scala
@@ -13,6 +13,8 @@ object ApplicationBuild extends Build {
     "com.typesafe.slick" % "slick-extensions_2.10" % "1.0.0",
     //"com.typesafe.play" %% "play-slick" % "0.4.0",
     "mysql" % "mysql-connector-java" % "5.1.26",
+    "org.apache.httpcomponents" % "httpclient" % "4.2.5",
+
     jdbc
   )
 
