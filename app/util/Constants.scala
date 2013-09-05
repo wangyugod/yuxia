fbad05230a76b633edb1322f40a292c18389c020
Simon Wang <wangyu4j@gmail.com>
2013-09-05 21:23:55 +0800

    add new for merchandising

---
+++ app/util/Constants.scala
@@ -0,0 +1,14 @@
+package util
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: simonwang
+ * Date: 9/5/13
+ * Time: 5:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object Constants {
+  //Merchant Register Type
+  val SELF_REGISTERED = "SELF_REG"
+  val ADMIN_REGISTERED = "ADMIN_REG"
+}
